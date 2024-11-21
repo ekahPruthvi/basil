797066e831ae2bfab0ea2c4532cf546d56a49515
ekahPruthvi <p1sribhat@gmail.com>
2024-11-21 12:17:46 +0530

    Footer

--- test.sh
+++ test.sh
@@ -5,12 +5,7 @@ source "basil"
 Line_sweep
 
 Head_text "Bob"
-
+echo ""
 Form "hello can u make me pasta" "yes ig i can" "this is second BOB"
 
-echo "
-
-
-
-"
-Foot_text 
+Foot
---
+++ testrun.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd ~/Documents/bashUI
+~/Documents/bashUI/test.sh
