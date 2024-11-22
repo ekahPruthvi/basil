c456430014c078ae84e8bd157b525764858f2e1f
ekahPruthvi <p1sribhat@gmail.com>
2024-11-22 07:30:36 +0530

    Form

--- test.sh
+++ test.sh
@@ -4,8 +4,12 @@ source "basil"
 
 Line_sweep
 
-Head_text "Bob"
+Head_text "Bob(Header)"
 echo ""
 Form "hello can u make me pasta" "yes ig i can" "this is second BOB"
+
+Fillaform "First parameter is Heading:" "This is fillable form," "Leave the heading black if u dont want heading" "you can give input in this and it'll be recoded"
+printf "Enter choice here: "
+read inp
 
 Foot
--- testrun.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd ~/Documents/bashUI
-~/Documents/bashUI/test.sh
