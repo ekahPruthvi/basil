447e8bb8a717e58634423377acc61f8144200808
ekahPruthvi <p1sribhat@gmail.com>
2024-11-22 07:52:08 +0530

    line_space

--- test.sh
+++ test.sh
@@ -8,8 +8,10 @@ Head_text "Bob(Header)"
 echo ""
 Form "hello can u make me pasta" "yes ig i can" "this is second BOB"
 
-Fillaform "First parameter is Heading:" "This is fillable form," "Leave the heading black if u dont want heading" "you can give input in this and it'll be recoded"
+Fillaform "First parameter is Heading:" "   " "This is fillable form," "Give three black spaces if u want to give linespace" "you can give input in this and it'll be recoded" 
 printf "Enter choice here: "
-read inp
+#read inp
+
+Linspace 3
 
 Foot
